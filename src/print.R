e04a1135903ed7f2ac89ba0fb8a681f3668b9370
Justin Marciszewski <justinjm@users.noreply.github.com>
2022-11-09 17:09:37 +0000

    submit build

--- src/print.R
+++ src/print.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 # printing test
 
 print("This is printed using the print function")
@@ -7,3 +8,6 @@ message("This is printed using the message function")
 cat("This is printed using the cat function")
 
 write("This is printed using the write function", stdout())
+
+# view environment vars
+Sys.getenv()
