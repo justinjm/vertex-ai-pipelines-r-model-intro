ae72d2103f5dec2ef2c19a7f80ac860f282131a3
Justin Marciszewski <justinjm@users.noreply.github.com>
2022-11-10 18:35:10 +0000

    initial commit - second example placeholder

---
+++ 02-r-script-params/src/print.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+# printing test
+
+print("This is printed using the print function")
+
+message("This is printed using the message function")
+
+cat("This is printed using the cat function")
+
+write("This is printed using the write function", stdout())
+
+# view environment vars
+Sys.getenv()
