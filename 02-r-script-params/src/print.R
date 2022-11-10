fafd238108cd91342683f8227ffe37503f32aa57
Justin Marciszewski <justinjm@users.noreply.github.com>
2022-11-10 21:15:13 +0000

    initial draft - prepare to build and test

--- 02-r-script-params/src/print.R
+++ 02-r-script-params/src/print.R
@@ -1,6 +1,17 @@
 #!/usr/bin/env Rscript
-# printing test
+# inputs
+args <- commandArgs(trailingOnly = TRUE)
+project_id <- args[1]
+region <- args[2]
+gcs_uri_in <- args[3]
+gcs_uri_out <- args[4]
 
+cat("project_id: ", project_id)
+cat("region: ", region)
+cat("gcs_uri_in: ", gcs_uri_in)
+cat("gcs_uri_out: ", gcs_uri_out)
+
+# printing test
 print("This is printed using the print function")
 
 message("This is printed using the message function")
